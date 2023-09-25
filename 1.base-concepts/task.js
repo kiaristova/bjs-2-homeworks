4f0471a1e039956e310dc58078f407937fb09e53
Ekaterina Karelina <kiaristova@gmail.com>
2023-09-25 22:15:35 +0300

    the first task is completely completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discriminant;
+	discriminant = (b ** 2) - (4 * a * c);
+	if (discriminant > 0) {
+		let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(x1);
+		arr.push(x2);
+	} else if (discriminant === 0) {
+		arr[0] = -b / (2 * a);
+	}
+	return arr;
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let percentFormat = percent / 100;
+	let percentMonths = percentFormat / 12;
+	let loanBody = amount - contribution;
+	let monthlyPayment = loanBody * (percentMonths + (percentMonths / (((1 + percentMonths) ** countMonths) - 1)));
+	let totalLoanAmount = monthlyPayment * countMonths;
+	let totalLoanAmountRound = +totalLoanAmount.toFixed(2);
+	return totalLoanAmountRound;
 }
